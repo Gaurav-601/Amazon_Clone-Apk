124697952aa70facfc65a9c4b074123109413782
Gaurav Tiwari <121477756+Gaurav-601@users.noreply.github.com>
2024-02-28 21:19:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StackNavigator from "./navigation/StackNavigator";
+import { Provider } from "react-redux";
+import store from "./store";
+import { ModalPortal } from "react-native-modals";
+
+export default function App() {
+  return (
+    <>
+      <Provider store={store}>
+        <StackNavigator />
+        <ModalPortal />
+      </Provider>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
